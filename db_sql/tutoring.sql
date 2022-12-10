e59ef6a5db48836dbb0a1412dcce0ddf47d90e60
Magdalena <kokanovic422@gmail.com>
2022-12-10 16:33:31 +0100

    ...

--- db_sql/tutoring.sql
+++ db_sql/tutoring.sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 09, 2022 at 01:14 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `tutoring`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messenger_messages`
---
 
 CREATE TABLE `messenger_messages` (
   `id` bigint(20) NOT NULL,
