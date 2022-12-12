63b0f52415a103162482a0aac61683da170b3faf
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-12-12 11:12:55 +0100

    services displayed for users

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Service;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class StaticController extends AbstractController
 {
     #[Route('/static', name: 'app_static')]
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response
+    { $service = $doctrine->getRepository(Service::class)->findAll();
         return $this->render('static/index.html.twig', [
-            'controller_name' => 'StaticController',
+            'service' => $service,
         ]);
     }
 }
