6e759606e204bd5bb3912d2e19a21690dd330fe9
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-12-12 10:15:44 +0100

    redirected the logout

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
 {
-    #[Route('/welcome', name: 'app_welcome')]
+    #[Route('/', name: 'app_welcome')]
     public function index(): Response
     {
         return $this->render('welcome/index.html.twig', [
