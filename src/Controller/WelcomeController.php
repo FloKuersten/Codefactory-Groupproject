d004fbd97573a807571ac55cf24297ed059d2cc4
MarcoVanJura <112858300+MarcoVanJura@users.noreply.github.com>
2022-12-14 02:22:52 +0100

    added fonts and twigs pages

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -15,4 +15,28 @@ class WelcomeController extends AbstractController
             'controller_name' => 'WelcomeController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('static/about.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('static/contact.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('static/login.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+    }
 }
