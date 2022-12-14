c9c6fdba4d6e1a841771cf5ef2fc0dd6b8315e16
CMusger <it.musger@gmail.com>
2022-12-14 14:57:11 +0100

    Fixed Php error

--- src/Controller/TutoringController.php
+++ src/Controller/TutoringController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class TutoringController extends AbstractController
 {
     #[Route('/tutoring', name: 'app_tutoring')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $todos = $doctrine->getRepository(TutoringController::class)->findAll();
+
         return $this->render('tutoring/tutoring.html.twig', [
             'controller_name' => 'TutoringController',
         ]);
